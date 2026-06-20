111f2dd0089a1aeccf6a76979308c3754a6eea3b
daud <assidiqidaud17@gmail.com>
2026-06-20 07:55:54 +0700

    feat:migrasi dari html ke php & implementasi modular header-footer

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+</main>
+
+<footer>
+    <p>
+        &copy; <span id="year"></span> Pustakaku Team. Seluruh Hak Cipta
+        Dilindungi.
+    </p>
+</footer>
+</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="id" dir="ltr">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Pustakaku - Belajar dari Pre-K hingga Kuliah</title>
+    <!-- style -->
+    <link rel="stylesheet" href="style.css" />
+    <!-- js -->
+    <script src="script.js"></script>
+</head>
+
+<body>
+    <header>
+        <h1>Selamat Datang di Pustakaku</h1>
+        <p>Platform materi pembelajaran lengkap dari Pre-K sampai Kuliah</p>
+    </header>
+
+    <main>
